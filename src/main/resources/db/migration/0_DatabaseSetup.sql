6d274291a262d6f0cc72f71774565f1934b46a9b
raj <raj@JLS>
2015-03-20 01:00:01 +0530

    commit

---
+++ src/main/resources/db/migration/0_DatabaseSetup.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Persons
+(
+PersonID int,
+DOB int,
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+Qualification varchar(255),
+College varchar(255),
+City varchar(255)
+);
